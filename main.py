d363b469b97f4c1b40c95cfb7405f298d2720f6d
Jennifer <jennifersequina@gmail.com>
2022-01-10 13:29:20 +0400

    updated

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-popup-blocking")
-
+print("Chromedriver installed and maximized")
 
 book_title = "Atomic Habits"
 driver.get("https://www.oceanofpdf.com")
+
+print("Ocean of PDF webpage opened")
 
 search_bar = driver.find_element(By.ID, "searchform-1")
 search_bar.send_keys(book_title)
 search_bar.send_keys(Keys.RETURN)
 
+print("Book title searched")
+
 result = driver.find_element(By.CLASS_NAME, "entry-title-link")
 result.click()
+
+print("Search result clicked")
 
 epub = driver.find_element(By.XPATH, "//div//input[@alt='Submit' and @src='https://oceanofpdf.com/wp-content/uploads/epub-button.jpg']")
 epub.send_keys()
 epub.click()
+
+print("EPUB clicked for download")
+
+time.sleep(15)
+driver.close()
+driver.quit()
+
+print("Check downloads folder")
